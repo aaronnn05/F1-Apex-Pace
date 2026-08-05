3a1ad6dca30e228c9b388ccc0e0f5544cb382fc6
Aaron <aaronchinosrs@gmail.com>
2026-08-05 16:13:20 +0800

    feat: create func to get laps data

--- src/apex_pace/features/pipeline.py
+++ src/apex_pace/features/pipeline.py
@@ -1,6 +1,38 @@
 """Polars feature transformation pipeline with point-in-time correctness."""
 
+from pathlib import Path
+
+import fastf1
 import polars as pl
+
+
+def get_laps_data():
+
+    # 1. Setup local cache directory
+    CACHE_DIR = Path(__file__).resolve().parents[3] / "data" / "raw" / "fastf1_cache"
+    CACHE_DIR.mkdir(parents=True, exist_ok=True)
+    fastf1.Cache.enable_cache(CACHE_DIR)
+
+    print("FastF1 cache enabled successfully.")
+
+    # 2. Load 2024 Bahrain Grand Prix Race Session
+    # Year: 2024, Grand Prix: 'Bahrain', Session: 'R' (Race)
+    session = fastf1.get_session(2024, "Bahrain", "R")
+    session.load(telemetry=False, weather=True)
+
+    print("Race session data loaded")
+
+    # 3. Extract laps dataframe
+    laps = session.laps
+
+    # 4. Convert Pandas df into Polars df
+    # Polars allow high-performance transformations
+    laps_pl = pl.from_pandas(laps)
+
+    # Convert LapTime from timedelta object to seconds
+    laps_pl = laps_pl.with_columns((pl.col("LapTime").dt.total_milliseconds() / 1000).alias("LapTimeSeconds"))
+
+    return laps_pl
 
 
 def build_feature_set(laps_df: pl.DataFrame) -> pl.DataFrame:
